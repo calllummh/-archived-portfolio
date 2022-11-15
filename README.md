1e6d42c16ffa8b1778725a4ebfe2a2401a5ead14
Callum Hargrove <callum.hargrove21@imperial.ac.uk>
2022-11-15 19:43:29 +0000

    complete without projects

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ welcome to my portfolio!
 
 through this project, i've dived into some web-dev, re-learning html, css and javascript and teaching myself scss and bootstrap.
 
+currently, it's a work in progress - optimised for desktop
+
 i hope you like it :)
+
 
---
+++ main.js
@@ -0,0 +1,9 @@
+// function about() {
+//     // var x = document.getElementById("about");
+//     // if (x.style.display === "none") {
+//     //   x.style.display = "block";
+//     // } else {
+//     //   x.style.display = "none";
+//     // }
+//     document.getElementById("subtitles").classList.toggle("show");
+// }
